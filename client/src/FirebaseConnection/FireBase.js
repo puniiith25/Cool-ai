64e01515c2a3dbf9da7f4a4aee5de377e9b6661a
puniiith25 <puniiith25@gmail.com>
2026-02-02 20:50:10 +0530

    ..

--- client/src/FirebaseConnection/FireBase.js
+++ client/src/FirebaseConnection/FireBase.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyA7OfkGuU8hz_0FGe0apOiUS4jqhFpyysk",
     authDomain: "cool-ai-96504.firebaseapp.com",
@@ -18,8 +14,8 @@ const firebaseConfig = {
     measurementId: "G-CJ3LCQMR5Z"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
