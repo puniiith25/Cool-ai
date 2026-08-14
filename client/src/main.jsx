38327da9261acdd529f20afbad9bd35d8369497e
puniiith25 <puniiith25@gmail.com>
2026-08-14 19:59:27 +0530

    Remove old authentication pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-import AuthProvider from './components/AuthContext.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,11 +13,11 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <BrowserRouter>
-      <AuthProvider>
+     
 
 
         <App />
-      </AuthProvider>
+     
     </BrowserRouter>
   </ClerkProvider>
 
